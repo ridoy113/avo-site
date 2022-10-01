cbde59b44e5d46d76e9208c100858a79278c86e2
ridoy113 <almamounhridoy113@gmail.com>
2022-10-01 16:22:53 +0600

    header making start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './sass/main.scss';
+import Header from "./Components/Header";
 
 
 function App() {
   return (
     <div>
-
+      <Header />
     </div>
   );
 }
